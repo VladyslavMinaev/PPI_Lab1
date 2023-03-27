4a15b0a5e3e381baab962d88c160b315afa703d0
Vladyslav <112310301+VladyslavMinaev@users.noreply.github.com>
2023-03-27 21:21:58 +0300

    Add files via upload

---
+++ Domain/ConsoleWriter.cs
@@ -0,0 +1,65 @@
+﻿using System.Timers;
+
+namespace PPI_Lab1.Domain;
+
+public class ConsoleWriter
+{
+    private string _name;
+    private System.Timers.Timer _timer;
+    private Thread _currentThread;
+    public ConsoleWriter(string name, int timerIntervalInMilliseconds)
+    {
+        _name = name;
+
+        var onIntervalElapsed = new ElapsedEventHandler(ShowThreadInformation);
+        _timer = new System.Timers.Timer(timerIntervalInMilliseconds);
+        _timer.AutoReset = true;
+        _timer.Elapsed += onIntervalElapsed;
+
+        _currentThread = null;
+    }
+
+    public void Start(object? o)
+    {
+        _currentThread = Thread.CurrentThread;
+
+        int iterationMaxValue;
+        try
+        {
+            iterationMaxValue = (int)o;
+        }
+        catch (InvalidCastException)
+        {
+            iterationMaxValue = 100000;
+            Console.WriteLine($"Invalid argument, method will run with {nameof(iterationMaxValue)}=100000");
+        }
+
+        _timer.Start();
+        for (int i = 1; i <= iterationMaxValue; i++)
+        {
+            Console.WriteLine($"Name: {_name}; State: {i}");
+            Thread.Sleep(200);
+        }
+
+        Thread.Sleep(10000);
+        Console.WriteLine($"The {_name} is shutting down ...");
+    }
+
+    private void ShowThreadInformation(object? o, ElapsedEventArgs e)
+    {
+        if (_currentThread == null)
+            throw new NullReferenceException($"{_currentThread} can not contain null");
+
+        var th = _currentThread;
+
+        Console.WriteLine("\n\n");
+        Console.WriteLine($"Thread name: {th.Name}");
+        Console.WriteLine("Managed thread #{0}: ", th.ManagedThreadId);
+        Console.WriteLine("Background thread: {0}", th.IsBackground);
+        Console.WriteLine("Thread pool thread: {0}", th.IsThreadPoolThread);
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write("Priority: {0}", th.Priority);
+        Console.ForegroundColor = ConsoleColor.Black;
+        Console.WriteLine("\n\n");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,103 @@
+﻿using PPI_Lab1.Domain;
+
+// var onIntervalElapsed = new ElapsedEventHandler(ShowThreadInformation);
+string firstThreadName = "Thread 1";
+string secondThreadName = "Thread 2";
+
+var writer1 = new ConsoleWriter(firstThreadName, 2000);
+var writer2 = new ConsoleWriter(secondThreadName, 2000);
+
+Thread thread1 = new Thread(new ParameterizedThreadStart(writer1.Start))
+{
+    Priority = ThreadPriority.Lowest
+};
+Thread thread2 = new Thread(new ParameterizedThreadStart(writer2.Start))
+{
+    Priority = ThreadPriority.AboveNormal
+};
+
+thread1.Start(100000);
+Thread.Sleep(500);
+thread2.Start(100000);
+
+await ThreadPriorityAsyncHandler();
+
+async Task ThreadPriorityAsyncHandler()
+{
+    await Task.Run(() =>
+    {
+        while (true)
+        {
+            var command = GenerateCommand();
+            if (string.IsNullOrEmpty(command) == true)
+                return;
+
+            var threadAndPriority = ParseCommand(command);
+            if (threadAndPriority == null)
+                return;
+
+            var thread = threadAndPriority.Item1;
+            var priority = threadAndPriority.Item2;
+
+            thread.Priority = priority;
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"The {command.Split(':')[0]} has been changed to priority: {priority}");
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Thread.Sleep(10000);
+        }
+    });
+}
+
+string? GenerateCommand()
+{
+    var value = Random.Shared.Next(0, 2);
+
+    if (value == 0)
+        return $"{firstThreadName}:";
+    else if (value == 1)
+        return $"{secondThreadName}:";
+
+    return string.Empty;
+}
+
+Tuple<Thread, ThreadPriority>? ParseCommand(string message)
+{
+    var parameters = message.Split(':');
+
+    if (parameters.Length != 2)
+        return null;
+
+    var threadToChange = GetTreadByName(parameters[0]);
+    if (threadToChange == null)
+        return null;
+
+    var newPriority = GetRandomThreadPriority();
+
+    return new Tuple<Thread, ThreadPriority>(threadToChange, newPriority);
+}
+
+Thread? GetTreadByName(string threadName)
+{
+    if (firstThreadName == threadName)
+        return thread1;
+    else if (secondThreadName == threadName)
+        return thread2;
+
+    return null;
+}
+
+ThreadPriority GetRandomThreadPriority()
+{
+    var value = Random.Shared.Next(0, 3);
+
+    if (value == 0)
+        return ThreadPriority.BelowNormal;
+    else if (value == 1)
+        return ThreadPriority.Normal;
+    else if (value == 2)
+        return ThreadPriority.AboveNormal;
+
+    return ThreadPriority.Lowest;
+}
